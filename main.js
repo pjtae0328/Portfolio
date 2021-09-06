b2532087cad528acd7df60dca409d63d3758ef02
pjtae0328 <pjtae0328@gmail.com>
2021-09-06 23:23:35 +0900

    JavaScript: Navbar

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+//넷바 스크롤 이벤트
+const home = document.getElementById('home');
+const navbar = document.getElementById('navbar');
+document.addEventListener('scroll', () => {
+    if(window.scrollY >= navbar.getBoundingClientRect().height) {
+        navbar.classList.add('navbar-dark');
+    } else {
+        navbar.classList.remove('navbar-dark');
+    }
+});
