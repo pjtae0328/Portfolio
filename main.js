ce8997370ebba879db90f64dc4b72e171f8d8be0
pjtae0328 <pjtae0328@gmail.com>
2021-09-07 01:30:00 +0900

    JavaScript: Home contact btn scroll handling

--- main.js
+++ main.js
@@ -10,16 +10,23 @@ document.addEventListener('scroll', () => {
     }
 });
 
-//넷바 스크롤 핸들링
+//넷바 버튼 스크롤 핸들링
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
     const link = target.dataset.link;
-
     if(link == null) {
         return;
     }
 
+    scrollTo(link);
+});
+
+function scrollTo(link) {
     const scrollTo = document.querySelector(link);
     scrollTo.scrollIntoView({behavior: "smooth"});
-})
+}
+
+//Contact 버튼 핸들링
+const btnContact = document.querySelector('.welcome__contact');
+btnContact.addEventListener('click', () => scrollTo('#contact'));
